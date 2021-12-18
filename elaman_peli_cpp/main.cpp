44eede83a608ddeb876d361dc2516094f3c93f6e
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2021-12-18 23:25:34 +0200

    Virheenkäsittely syntymäpaikan valintaan, lapsuuden tulostus
    Syöte otetaan vastaaan merkkijono. Se for-loopataan läpi, ja jos jostain indeksistä löytyy muu kuin luku, tulostetaan virhe. Jos merkkijono sisältää pelkkiä lukuja, muunnetaan se std::stoilla kokonaisluvuksi.
    
    Tehty myös lapsuuden printtaus joka syntymäpaikkaan (omat funktionsa).

--- elaman_peli_cpp/main.cpp
+++ elaman_peli_cpp/main.cpp
@@ -1,28 +1,106 @@
-#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 
 using std::string;
 
-void firstQuestions (string name, string placeOfBirth, std::ostream& output)
+bool isInputValid (string placeOfBirth)
+{
+    for (unsigned i = 0; i < placeOfBirth.length(); i++)
+    {
+        if (!isdigit(placeOfBirth[i]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void askName (string& name, std::ostream& output)
 {
     output << "What's your name?: ";
     std::getline(std::cin, name);
-    output << "\n" << name << " is a nice name!" << std::endl;
 
-    output << "Select your place of birth: " << "\n" << std::endl
-              << "1: Helsinki" << std::endl
-              << "2: Tampere" << std::endl
-              << "3: Turku" << std::endl;
-    std::getline(std::cin, placeOfBirth);
+    output << name << " is a nice name!" << std::endl;
+}
+
+void childhood(std::ostream& output)
+{
+    output << "\n" << "Your childhood goes by..." << std::endl;
+
+    for (unsigned i = 0; i <= 15; i += 5)
+    {
+        output << "You're now " << i << " years old" << std::endl;
+    }
+
+    output << std::endl;
+}
+
+void routeHelsinki(std::ostream& output)
+{
+    output << "Hei Helsingista";
+    childhood(output);
+}
+
+void routeTampere(std::ostream& output)
+{
+    output << "Hei Tampereelta";
+    childhood(output);
+}
+
+void routeTurku(std::ostream& output)
+{
+    output << "Hei Turusta";
+    childhood(output);
+}
+
+void askPlaceOfBirth (string placeOfBirth, std::ostream& output)
+{
+    while (true)
+    {
+        output << "\nSelect your place of birth: " << std::endl
+                  << "1: Helsinki" << std::endl
+                  << "2: Tampere" << std::endl
+                  << "3: Turku" << std::endl;
+        std::getline(std::cin, placeOfBirth);
+
+        bool isValueValid = isInputValid(placeOfBirth);
+        if (!isValueValid)
+        {
+            output << "\nERROR: YOU DIDN'T INPUT AN INTEGER. PLEASE TYPE A NEW VALUE." << std::endl;
+            continue;
+        }
+
+        else
+        {
+            break;
+        }
+    }
+
+    output << "\nWe got here\n";
+
+    int oper = 0;
+    oper = std::stoi(placeOfBirth);
+
+    switch (oper)
+    {
+        case 1:
+            routeHelsinki(output);
+            break;
+        case 2:
+            routeTampere(output);
+            break;
+        case 3:
+            routeTurku(output);
+            break;
+    }
 }
 
 int main()
 {
-    //VARIABLES
-
-    std::ostream& output = std::cout;
-    std::ifstream instructions("instructions.txt");
-
     //STRINGS
     string job1;
     string job2;
@@ -34,14 +112,19 @@ int main()
     string secondarySchool;
     string studies;
 
+    //VARIABLES
+
+    std::ostream& output = std::cout;
+    std::ifstream instructions("instructions.txt");
+
+
     if (instructions.is_open())
     {
-        output << instructions.rdbuf() // Prints the contents of the file
-               << std::endl
-               << std::endl;
+        output << instructions.rdbuf() << std::endl; // Prints the contents of the file
     }
 
-    firstQuestions(name, placeOfBirth, output);
+    askName(name, output);
+    askPlaceOfBirth(placeOfBirth, output);
 
     return 0;
 }

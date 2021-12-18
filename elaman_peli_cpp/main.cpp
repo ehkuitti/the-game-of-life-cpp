e9bc8eb4ab1470a08cd0def087bbe6c155997b00
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2021-12-18 15:47:57 +0200

    Ohjeet tiedostoon, alun kysymyksille oma funktio
    - C++17
    - using std.:string
    - Luotu uusi funktio firstQuestions
    - Peliohjeet omassa tiedostossaan ja niiden luku (#include <fstream>)

--- elaman_peli_cpp/main.cpp
+++ elaman_peli_cpp/main.cpp
@@ -1,39 +1,72 @@
 #include <iostream>
+#include <fstream>
+
+using std::string;
+
+void firstQuestions (string name, string placeOfBirth, std::ostream& output)
+{
+    output << "What's your name?: ";
+    std::getline(std::cin, name);
+    output << "\n" << name << " is a nice name!" << std::endl;
+
+    output << "Select your place of birth: " << "\n" << std::endl
+              << "1: Helsinki" << std::endl
+              << "2: Tampere" << std::endl
+              << "3: Turku" << std::endl;
+    std::getline(std::cin, placeOfBirth);
+}
 
 int main()
 {
-    //MUUTTUJAMÄÄRITTELYT
+    //VARIABLES
 
-    //Stringit
-    std::string syntymapaikka;
-    std::string nimi;
-    std::string ekaAste;
-    std::string tokaAste;
-    std::string opinnot;
-    std::string tyo1;
-    std::string tikoTyo;
-    std::string tyo2;
-    std::string sosiaaliTyo;
+    std::ostream& output = std::cout;
+    std::ifstream instructions("instructions.txt");
 
-    std::cout << "Tervetuloa pelaamaan Tomte™ "
-                 "Corperationin®\n\n\t\tELÄMÄN_PELIÄ©    :)\n" << std::endl
-              << "Ohjeet: Vedä aluksi console IDE:ssäsi "
-                 "niin isoksi kuin mahdollista. Tämä helpottaa pelaamista "
-                 "huomattavasti. Tämän jälkeen klikkaa \"Anna nimesi\" -rivin "
-                 "lopusta tyhjältä alueelta.\nVastaa pelissä kirjoittamalla "
-                 "numero tai sana pyydetyn mukaisesti ja painamalla sitten "
-                 "enteriä. Hyviä pelihetkiä toivottaa Tomten väki!\n"
-              << std::endl;
+    //STRINGS
+    string job1;
+    string job2;
+    string job_bachelorOfBusinessIT;
+    string job_socialWork;
+    string name;
+    string placeOfBirth;
+    string primarySchool;
+    string secondarySchool;
+    string studies;
 
-    std::cout << "Syötä nimesi: ";
-    std::getline(std::cin, nimi);
-    std::cout << "\n" << nimi << " on hieno nimi!" << "\n" << std::endl;
+    if (instructions.is_open())
+    {
+        output << instructions.rdbuf() // Prints the contents of the file
+               << std::endl
+               << std::endl;
+    }
 
-    std::cout << "Valitse syntymäpaikkasi: " << "\n" << std::endl
-              << "1: Vantaa" << std::endl
-              << "2: Oulu" << std::endl
-              << "3: Turku" << std::endl;
-    std::getline(std::cin, syntymapaikka);
+    firstQuestions(name, placeOfBirth, output);
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    output << "Welcome to Tomte™ "
+//              "Corperation's®\n\n\t\tGAME_OF_LIFE©    :)\n"
+//           << std::endl
+//           << "Instructions: First of all, please maximize the console view in "
+//              "your IDE. This makes it much easier to play. "
+//              "After this click the space after \"What's your name?\""
+//              "\nUse numbers or words to answer the question and press enter to"
+//              "submit your answer.  "
+//              "Have fun and best regards from Korporate Staff!\n"
+//           << std::endl;
